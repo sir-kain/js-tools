258c213d248b2324998a1b6c737c49aa6890a3cf
Waly <ahmadouwalyndiaye@gmail.com>
2024-05-04 14:11:00 +0000

    Explain minify

---
+++ src/main.js
@@ -0,0 +1,62 @@
+const names = [
+  "Ahmadou Waly Ndiaye",
+  "Adji Fatma Mbacke Thioune",
+  "23 a33",
+  "Moustapha Gueye",
+  "Ndeye Binta",
+  " ko",
+  "Inno",
+];
+
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function $$(selector) {
+  return document.querySelectorAll(selector);
+}
+
+function createNode(element) {
+  return document.createElement(element);
+}
+
+function append(parent, el) {
+  parent.insertAdjacentElement("beforeend", el);
+}
+
+function nameInitals(names) {
+  return names.map((fullname) => {
+    return fullname.split(" ").map((name) => name.charAt(0));
+  });
+}
+
+function maxInitialNumber(initials, n) {
+  return initials.map((inital) => inital.slice(0, n));
+}
+
+// $(".names pre").innerHTML = JSON.stringify(names);
+
+function item(initials) {
+  const $item = createNode("article");
+  $item.classList.add("item");
+  $item.style.background =
+    "#" + Math.floor(Math.random() * 16777215).toString(16);
+  initials.forEach((initial) => {
+    const $span = createNode("span");
+    $span.textContent = initial;
+    append($item, $span);
+  });
+
+  return $item;
+}
+
+function items(initialList) {
+  const $items = $(".items");
+  initialList.forEach((initials) => {
+    const $item = item(initials);
+    append($items, $item);
+  });
+}
+
+const initialList = maxInitialNumber(nameInitals(names), 3);
+items(initialList);
